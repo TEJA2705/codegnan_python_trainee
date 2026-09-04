e534c58595ca7be2aa7ddf2a137b52e3dc389c5a
TEJA2705 <vkdteja02@gmail.com>
2026-09-04 15:29:27 +0530

    Add files via upload

---
+++ Operators-sep4.py
@@ -0,0 +1,133 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=4
+b=2
+print(a+b)
+6
+print(a-b)
+2
+print(a/b)
+2.0
+print(a//b)
+2
+print(a%b)
+0
+print(a**b)
+16
+print(a*b)
+8
+#Assignment
+b+=2
+b
+4
+b-=2
+b
+2
+b*=2
+b
+4
+b/=2
+b
+2.0
+b//=3
+b
+0.0
+b%=2
+b
+0.0
+b**=3
+b
+0.0
+a+=2
+a
+6
+a-=3
+a
+3
+a*=2
+a
+6
+a/=2
+a
+3.0
+a//=2
+a
+1.0
+a**=2
+a
+1.0
+a%=1
+a
+0.0
+#Comparision
+a=10
+b=8
+a>b
+True
+a<b
+False
+a!=b
+True
+a==b
+False
+a>=b
+True
+a<=b
+False
+#Logical Operator
+a>b and a<b
+False
+a>b and a<b
+False
+a>b or b>a
+True
+a<b or a<b
+False
+a>b or a<b
+True
+not False
+True
+not True
+False
+#Identity
+a=4
+type(a) is int
+True
+type(a) is not int
+False
+b=3.4
+type(b) is not
+SyntaxError: invalid syntax
+type(b) is not float
+False
+type(b) is float
+True
+#Membership
+a=[1,2,3,4]
+b=3
+>>> b in a
+True
+>>> b not in a
+False
+>>> #Bitwise Operator
+>>> a=2
+>>> b=5
+>>> a&b
+0
+>>> bin(a)
+'0b10'
+>>> bin(b)
+'0b101'
+>>> a|b
+7
+>>> ~a
+-3
+>>> ~b
+-6
+>>> a^b
+7
+>>> a=4
+>>> a<<2
+16
+>>> a>>2
+1

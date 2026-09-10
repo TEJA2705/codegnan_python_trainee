62303f185d2969b7479f6d525b11ca7e51095a15
TEJA2705 <vkdteja02@gmail.com>
2026-09-10 20:30:28 +0530

    Add files via upload

---
+++ runtime_input_sep10.py
@@ -0,0 +1,42 @@
+#run_time input()
+#Integer
+'''a=int(input("Enter num1: "))
+b=int(input("Enter num2: "))
+print(a+b)'''
+
+#float
+'''a=float(input("Enter num1: "))
+b=float(input("Enter num2: "))
+print(a+b)'''
+
+#String -> we can use input or str(input())also
+'''a=input("Enter string1: ")
+b=input("Enter string2: ")
+print(a+" "+b)'''
+
+#complex
+'''a=complex(input("Enter num1: "))
+b=complex(input("Enter num2: "))
+print(a+b)'''
+
+#Boolean
+'''a=bool(input("Enter no1: "))
+b=bool(input("Enter no2: "))
+c=bool(input("Enter no3: "))
+print(a+b+c)'''
+
+a=int(input("Enter num1: "))
+b=int(input("Enter num2: "))
+option=int(input("choose the option:\n1.Addition\n2.Subtraction\n3.Multiple\t"))
+print(a+b)
+print(a-b)
+print(a*b)
+
+'''a=int(input("Enter num1: "))
+b=int(input("Enter num2: "))
+option=input("choose the option:\n1.Addition\n2.Subtraction\n3.Multiple\t")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
